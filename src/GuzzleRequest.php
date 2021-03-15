b890f8f5200baee695da37d89d8a5698c9e140a5
maksimovvp <maksimovvp@gmail.com>
2021-03-15 07:39:21 +0300

    Add contract implementation.

---
+++ src/GuzzleRequest.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+
+namespace Library;
+
+use GuzzleHttp\Exception\GuzzleException;
+use JsonException;
+use GuzzleHttp\Client;
+
+class GuzzleRequest implements ApiRequest
+{
+    private string $apiUrl;
+
+    /**
+     * GuzzleApiClient constructor.
+     *
+     * @param string $apiUrl
+     */
+    public function __construct(string $apiUrl)
+    {
+        $this->apiUrl = $apiUrl;
+    }
+
+    /**
+     * @param string $requestType
+     * @param string $apiMethod
+     * @param array $queryParams
+     * @param array $postParams
+     *
+     * @return array
+     *
+     * @throws ApiRequestException
+     */
+    public function sendRequest(
+        string $requestType,
+        string $apiMethod,
+        array $queryParams,
+        array $postParams = []
+    ): array
+    {
+        $json = $this->tryToSendRequest(
+            $requestType,
+            $this->apiUrl . $apiMethod . '?' . http_build_query($queryParams),
+            $postParams
+        );
+
+        return $this->tryToDecode($json);
+    }
+
+    /**
+     * @param string $requestType
+     * @param string $requestUrl
+     * @param array $postParams
+     *
+     * @return string
+     *
+     * @throws ApiRequestException
+     */
+    private function tryToSendRequest(
+        string $requestType,
+        string $requestUrl,
+        array $postParams
+    ): string
+    {
+        $client = new Client();
+
+        try {
+            $res = $client->request(
+                $requestType,
+                $requestUrl,
+                $postParams
+            );
+
+            return $res->getBody()->getContents();
+        } catch (GuzzleException $e) {
+            throw new ApiRequestException($e->getMessage());
+        }
+    }
+
+    /**
+     * @param string $json
+     *
+     * @return array
+     *
+     * @throws ApiRequestException
+     */
+    private function tryToDecode(string $json): array
+    {
+        try {
+            return json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            throw new ApiRequestException($e->getMessage());
+        }
+    }
+}
